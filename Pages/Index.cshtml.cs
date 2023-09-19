9042fb0bc33d05f667f564d6679b4fcd8877f4ff
FraserCopland <fcopland@my.bcit.ca>
2023-09-18 23:11:09 -0700

    files made

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Chinese_Zodiac.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chinese_Zodiac.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    
+    public int Year {get; set;}
+    public void OnGet()
+    {
+        ViewData["Zodiac"] = "";
+        ViewData["Image"] = "";
+    }
+
+    public void OnPost(Util zodiacYear)
+    {
+        if (Year < 1900 || Year > DateTime.Now.Year + 1){
+            ViewData["Zodiac"] = "Invalid Year";
+        } else {
+            ViewData["Zodiac"] = zodiacYear.GetZodiac();
+        ViewData["Image"] = "../images/" + zodiacYear.GetZodiac().ToLower() + ".png";
+        }
+        
+    }
+}
